f0fb7e6e3978680211507f5303d79919fdf9c37d
wintergalt <wintergalt@gmail.com>
2012-07-01 22:18:59 -0300

    Now get last message from a given newsgroup is working

--- src/model/utils.py
+++ src/model/utils.py
@@ -14,24 +14,20 @@ class CanillaUtils():
         return prefs.default_server
         
     def get_subscribed_groups(self, ns):
-        logging.fatal('Inside get_subscribed_newsgroups')
+        logging.debug('Inside get_subscribed_newsgroups')
         if not ns:
             ns = self.get_default_server()
         return self.sql_session.query(Newsgroup).filter_by(newsserver=ns).filter_by(subscribed=True).all()
     
     def retrieve_stored_messages(self, ng):
-        logging.fatal('Inside retrieve_stored_headers')
+        logging.debug('Inside retrieve_stored_headers')
         return ng.messages
     
     def get_last_stored_message(self, ng):
-        '''
-        return self.sql_session.query(Message) \
-            .filter(Message.number==func.max(Message.number).select())\
-            .first()
-        '''
-        
-        #return self.sql_session.query(Message).join(ng.messages).order_by(desc(Message.number)).first()
-        return self.sql_session.query(ng.messages).order_by(desc(Message.number)).first()
+        logging.debug('Inside get_last_stored_message(%s)' % ng.name)
+        return self.sql_session.query(Message)\
+            .filter(Message.newsgroups.comparator.contains(ng))\
+            .order_by(desc(Message.number)).first()
     
     
     
